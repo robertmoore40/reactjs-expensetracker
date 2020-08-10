73c958a04a6e4812985159dd657bdf484e90bc10
robertmoore40 <robertcmoore92@gmail.com>
2020-08-10 13:42:33 -0400

    added return

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -8,3 +8,5 @@ export const Balance = () => {
     const amounts = transactions.map(transaction => transaction.amount);
 
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    return (
