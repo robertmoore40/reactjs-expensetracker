f70a56c2f0cf33f044722c78c0f6bc44e45bd4a1
robertmoore40 <robertcmoore92@gmail.com>
2020-08-11 10:27:29 -0400

    reducers

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -8,3 +8,6 @@ export const IncomeExpenses = () => {
     const amounts = transactions.map(transaction => transaction.amount);
 
     const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
