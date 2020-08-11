78448846eb61a0ba2a51c290d38ea597f648c095
robertmoore40 <robertcmoore92@gmail.com>
2020-08-11 10:22:48 -0400

    finished balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -10,3 +10,9 @@ export const Balance = () => {
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
     return (
+        <>
+        <h4>Your Balance</h4>
+      <h1>${total}</h1>
+      </>
+    )
+  }
