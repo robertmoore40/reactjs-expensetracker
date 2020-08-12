e3b3284c49a89632f2644cf154e7e87ae6bb8bf7
robertmoore40 <robertcmoore92@gmail.com>
2020-08-12 07:29:27 -0400

    added money - income

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -21,3 +21,8 @@ export const IncomeExpenses = () => {
       <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
+
+          <p className="money plus">{income}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
