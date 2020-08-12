d3a0f7aeee268fd6f545d5bfb34cdf07e74d3f59
robertmoore40 <robertcmoore92@gmail.com>
2020-08-12 07:29:56 -0400

    finished income expenses

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -26,3 +26,9 @@ export const IncomeExpenses = () => {
         </div>
         <div>
           <h4>Expense</h4>
+
+          <p className="money minus">{expense}</p>
+        </div>
+      </div>
+  )
+}
