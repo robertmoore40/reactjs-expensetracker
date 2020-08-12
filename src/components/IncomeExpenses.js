89437f1d03e7f6a966f57a701ecf9ef0ed6cb07a
robertmoore40 <robertcmoore92@gmail.com>
2020-08-12 07:28:03 -0400

    fixed expense

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -13,7 +13,6 @@ export const IncomeExpenses = () => {
     .toFixed(2);
 
     const expense = (
-        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-        -1
-      ).toFixed(2);
-   
+      amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+      -1
+    ).toFixed(2);
