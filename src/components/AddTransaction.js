85473c7426457efb588bc137b3f4f4a641a1e04c
robertmoore40 <robertcmoore92@gmail.com>
2020-08-14 17:27:40 -0400

    form

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -28,3 +28,8 @@ export const AddTransaction = () => {
     <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
