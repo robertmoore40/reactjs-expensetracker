ae217caf7f226a513adaf6b52b2896ffa75733dd
robertmoore40 <robertcmoore92@gmail.com>
2020-08-15 12:20:12 -0400

    added history

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -6,3 +6,5 @@ import { GlobalContext } from '../context/GlobalState';
 export const TransactionList = () => {
     const { transactions } = useContext(GlobalContext);
     return (
+        <>
+        <h3>History</h3>
