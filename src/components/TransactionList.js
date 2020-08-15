7a99b39e06706cf9029ca3ac4fd37bed6a58e24e
robertmoore40 <robertcmoore92@gmail.com>
2020-08-15 12:20:35 -0400

    finished transaction history

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -8,3 +8,10 @@ export const TransactionList = () => {
     return (
         <>
         <h3>History</h3>
+
+        <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+    </>
+  )
+}
