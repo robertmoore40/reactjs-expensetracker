027701fa08587d0871803dfc4aebf9c77c8056d0
robertmoore40 <robertcmoore92@gmail.com>
2020-08-15 12:24:25 -0400

    app.css added

--- src/App.js
+++ src/App.js
@@ -7,3 +7,5 @@ import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
 import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
