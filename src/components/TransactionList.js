367d7b1dfcd1704c13091f13ffd13e00848a0e79
robertmoore40 <robertcmoore92@gmail.com>
2020-08-15 12:18:21 -0400

    add transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,3 +2,5 @@ import React, { useContext } from 'react';
 import { Transaction } from './Transaction';
 
 import { GlobalContext } from '../context/GlobalState';
+
+export const TransactionList = () => {
