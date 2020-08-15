f7b1f60ae65ba0cc43b6e788a48cc2cbc2044f26
robertmoore40 <robertcmoore92@gmail.com>
2020-08-15 12:16:45 -0400

    readjusted

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -16,19 +16,16 @@ export const IncomeExpenses = () => {
       amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
       -1
     ).toFixed(2);
-
     return (
       <div className="inc-exp-container">
-        <div>
-          <h4>Income</h4>
-
-          <p className="money plus">{income}</p>
+          <div>
+            <h4>Income</h4>
+    <p className="money plus">{income}</p>
+          </div>
+          <div>
+            <h4>Expense</h4>
+    <p className="money minus">{expense}</p>
+          </div>
         </div>
-        <div>
-          <h4>Expense</h4>
-
-          <p className="money minus">{expense}</p>
-        </div>
-      </div>
-  )
-}
+    )
+  }
