0491b17bbb0119c6a992d59c2c064533443b8c34
robertmoore40 <robertcmoore92@gmail.com>
2020-08-15 12:24:08 -0400

    add global state

--- src/App.js
+++ src/App.js
@@ -5,3 +5,5 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
