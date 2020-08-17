76128fef53bac88339b835d65c9cd7e78f0bb573
robertmoore40 <robertcmoore92@gmail.com>
2020-08-16 23:06:27 -0400

    added morgan use

--- server.js
+++ server.js
@@ -14,3 +14,8 @@ const transactions = require('./routes/transactions');
 const app = express();
 
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+  
