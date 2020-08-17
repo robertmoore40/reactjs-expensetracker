98f1174b25095bd45f8189df8903d3f0ff8f7cc7
robertmoore40 <robertcmoore92@gmail.com>
2020-08-16 23:03:48 -0400

    added config

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
