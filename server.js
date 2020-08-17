7c4f86e2b77fc35c88cc2a6ad9bad2c9bf9d0eb3
robertmoore40 <robertcmoore92@gmail.com>
2020-08-16 23:09:13 -0400

    app listen

--- server.js
+++ server.js
@@ -33,3 +33,5 @@ if(process.env.NODE_ENV === 'production') {
 
 
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
