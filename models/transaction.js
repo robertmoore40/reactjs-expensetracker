3a8c2539426300397de8010cd730fee9fac3494c
robertmoore40 <robertcmoore92@gmail.com>
2020-08-17 08:53:54 -0400

    created

--- models/transaction.js
+++ models/transaction.js
@@ -11,3 +11,8 @@ const TransactionSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'Please add a positive or negative number']
       },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    });
