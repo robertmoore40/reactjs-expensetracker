4f0901698c99033184dacb82a81151f41fcf0662
robertmoore40 <robertcmoore92@gmail.com>
2020-08-16 23:03:26 -0400

    connect to mongo

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
