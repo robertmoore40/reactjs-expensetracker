ac9fd2bf0bd362e5a352283977686c8f1e712f1e
robertmoore40 <robertcmoore92@gmail.com>
2020-08-18 09:06:06 -0400

    app function

--- client/App.js
+++ client/App.js
@@ -9,3 +9,17 @@ import { AddTransaction } from './components/AddTransaction';
 import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
+
+function App() {
+    return (
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    );
+  }
